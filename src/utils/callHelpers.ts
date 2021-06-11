2dca1696147ad8e612da62d631f6ff08a63de41e
hachiojidev <hachiojidev@protonmail.com>
2021-06-11 14:48:37 +0900

    refactor: Add helper to get active pools given block number
    - Removed deprecated call helpers

--- src/utils/callHelpers.ts
+++ src/utils/callHelpers.ts
@@ -1,7 +1,12 @@
 import BigNumber from 'bignumber.js'
 import { DEFAULT_GAS_LIMIT, DEFAULT_TOKEN_DECIMAL } from 'config'
 import { ethers } from 'ethers'
-import { BIG_TEN, BIG_ZERO } from './bigNumber'
+import { getAddress } from 'utils/addressHelpers'
+import pools from 'config/constants/pools'
+import sousChefABI from 'config/abi/sousChef.json'
+import { BIG_TEN } from './bigNumber'
+import { multicallv2 } from './multicall'
+import { web3WithArchivedNodeProvider } from './web3'
 
 const options = {
   gasLimit: DEFAULT_GAS_LIMIT,
@@ -83,8 +88,49 @@ export const soushHarvest = async (sousChefContract) => {
   return receipt.status
 }
 
-export const soushHarvestBnb = async (sousChefContract) => {
-  const tx = await sousChefContract.deposit({ ...options, value: BIG_ZERO })
-  const receipt = await tx.wait()
-  return receipt.status
+export const getActivePools = async (block?: number) => {
+  const archivedWeb3 = web3WithArchivedNodeProvider
+  const eligiblePools = pools
+    .filter((pool) => pool.sousId !== 0)
+    .filter((pool) => pool.isFinished === false || pool.isFinished === undefined)
+  const blockNumber = block || (await archivedWeb3.eth.getBlockNumber())
+  const multiCallOptions = {
+    web3: archivedWeb3,
+    requireSuccess: false,
+    blockNumber,
+  }
+
+  const startBlocks = (await multicallv2(
+    sousChefABI,
+    eligiblePools.map(({ contractAddress }) => {
+      return {
+        address: getAddress(contractAddress),
+        name: 'startBlock',
+      }
+    }),
+    multiCallOptions,
+  )) as MultiCallV2Result<BigNumber>[]
+  const endBlocks = (await multicallv2(
+    sousChefABI,
+    eligiblePools.map(({ contractAddress }) => {
+      return {
+        address: getAddress(contractAddress),
+        name: 'bonusEndBlock',
+      }
+    }),
+    multiCallOptions,
+  )) as MultiCallV2Result<BigNumber>[]
+
+  return eligiblePools.reduce((accum, pool, index) => {
+    const { data: startBlockData } = startBlocks[index]
+    const { data: endBlockData } = endBlocks[index]
+    const startBlock = new BigNumber(startBlockData[0]._hex)
+    const endBlock = new BigNumber(endBlockData[0]._hex)
+
+    if (startBlock.gt(blockNumber) || endBlock.lt(blockNumber)) {
+      return accum
+    }
+
+    return [...accum, pool]
+  }, [])
 }
